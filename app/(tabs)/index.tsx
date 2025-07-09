cc51dc1a961341cbdc0140c3e9c6807a69b18d37
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-09 18:33:49 +0600

    Create a tap navigation bar for the movie app

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-primary">
+  <Text className="text-white text-2xl font-bold">Welcome</Text>
+</View>
+
+  );
+}
