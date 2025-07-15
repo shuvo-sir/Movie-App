cfbc577d60c9eaf337e72b67ac2acab398e08e07
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-15 21:11:59 +0600

    Create a search bar in the search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,75 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
 import React from 'react'
+import { images } from '@/constants/images'
+import useFetch from '@/services/useFetch';
+import { useRouter } from 'expo-router';
+import { fetchMovies } from '@/services/api';
+import MovieCard from '@/components/MovieCard';
+import { icons } from '@/constants/icons';
+import SearchBar from '@/components/SearchBar';
 
-const search = () => {
+
+const Search = () => {
+
+  const router = useRouter();
+
+  const { 
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError
+   } = useFetch(() => fetchMovies({
+    query: ''
+  }))
+  
   return (
-    <View>
-      <Text>search</Text>
+    <View className=' flex-1 bg-primary'>
+      <Image
+        source={images.bg}
+        className=' flex-1 absolute w-full z-0'
+        resizeMode='cover'
+      />
+
+      <FlatList
+        data={movies}
+        renderItem={({item}) => <MovieCard {...item}/>}
+        keyExtractor={(item) => item.id.toString()}
+        className=' px-5'
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{paddingBottom: 100}}
+
+        ListHeaderComponent={
+          <>
+              <View className=' w-full flex-row justify-center mt-20 items-center'>
+                  <Image
+                    source={icons.logo}
+                    className=' w-12 h-10'
+                  />
+              </View>
+
+              <View className=' my-5'>
+                <SearchBar placeholder='Search movies...'/>
+              </View>
+
+              {moviesLoading && (
+                <ActivityIndicator size='large' color="#0000ff"
+                  className="my-3" />
+              )}
+
+              {moviesError && (
+                <Text className=' text-red-500 px-5 my-3'>
+                  Error: {moviesError.message}
+                </Text>
+              )}
+          </>
+        }
+      />
     </View>
   )
 }
 
-export default search
+export default Search
