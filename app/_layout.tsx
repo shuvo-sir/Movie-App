cb6e251960bcf29a56f025be2bdf2af5ea7d753f
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-18 23:17:24 +0600

    Hide the statusbar info like bettery percentage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,21 @@
 import { Stack } from "expo-router";
 import './global.css';
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen 
-      name="(tabs)"
-      options={{headerShown: false}}
-    />
-    <Stack.Screen 
-      name="movie/[id]"
-      options={{headerShown: false}}
-    />
+  return (
+    <>
+      <StatusBar hidden={true}/>
+    <Stack>
+      <Stack.Screen 
+        name="(tabs)"
+        options={{headerShown: false}}
+      />
+      <Stack.Screen 
+        name="movie/[id]"
+        options={{headerShown: false}}
+      />
   </Stack>
+  </>
+  );
 }
