902ce6a315679a2edd0ecd38280f8963ad41a715
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-19 22:59:58 +0600

    create the rating and votes of the movie

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import useFetch from '@/services/useFetch';
 import { fetchMovieDetails } from '@/services/api';
+import { icons } from '@/constants/icons';
 
 const MovieDetails = () => {
   const {id} = useLocalSearchParams();
@@ -16,8 +17,27 @@ const MovieDetails = () => {
         <View>
           <Image
             source={{uri: `http://image.tmdb.org/t/p/w500${movie?.poster_path}`}}
-            className=''
+            className=' w-full h-[550px]'
+            resizeMode='stretch'
           />
+        </View>
+
+        <View className=' flex-col items-start justify-center mt-5 px-5'>
+          <Text className=' text-white font-bold text-xl'>{movie?.title}</Text>
+
+          <View className=' flex-row items-center gap-x-1 mt-2'>
+            <Text className=' text-light-200 text-sm'>{movie?.release_date?.split('-')[0]}</Text>
+            <Text className=' text-light-200 text-sm'>{movie?.runtime}m</Text>
+          </View>
+          
+          <View className=' flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2'>
+            <Image
+              source={icons.star}
+              className=' size-4'
+            />
+            <Text className=' text-white font-bold text-sm'>{Math.round(movie?.vote_average ?? 0)}/10</Text>
+            <Text className=' bg-light-200 text-sm'>({movie?.vote_count} votes)</Text>
+          </View>
         </View>
       </ScrollView>
     </View>
