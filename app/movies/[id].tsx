0580a8dfaf0be3fdc94c3ab3f7e6ae22d32f8b32
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-19 22:36:32 +0600

    create details page

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import useFetch from '@/services/useFetch';
+import { fetchMovieDetails } from '@/services/api';
 
 const MovieDetails = () => {
+  const {id} = useLocalSearchParams();
+
+  const {data: movie, loading} = useFetch(() => fetchMovieDetails(id as string));
   return (
-    <View>
-      <Text>MovieDetails</Text>
+    <View className=' bg-primary flex-1'>
+      <ScrollView contentContainerStyle={{
+        paddingBottom: 80
+      }}>
+        <View>
+          <Image
+            source={{uri: `http://image.tmdb.org/t/p/w500${movie?.poster_path}`}}
+            className=''
+          />
+        </View>
+      </ScrollView>
     </View>
   )
 }
