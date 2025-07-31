42119a8f264605499ccc00229bbe011eefbc31c7
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-31 16:11:44 +0600

    b

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -52,6 +52,7 @@ export const updateSearchCount = async(query: string, movie: Movie) => {
     }
 }
 
+
 export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> => {
     try {
         const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
